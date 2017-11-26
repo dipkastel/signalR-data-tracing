49c035720a6ac31547308c9ccbef5cc1d8baa022
Amir Ashrafi <dipkastel@gmail.com>
2017-11-26 22:27:58 +0330

    add a usable simple project

---
+++ signalR-data-tracing/signalR-data-tracing/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signalR_data_tracing.Startup))]
+
+namespace signalR_data_tracing
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
